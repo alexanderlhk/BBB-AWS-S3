de8ebea8bde4b8e447bd9b648bdcdef7fc38312b
Alexander Lim <alexanderlhk@gmail.com>
2020-10-11 16:57:52 +0800

    Update code

--- bbb-s3.py
+++ bbb-s3.py
@@ -3,7 +3,7 @@ from botocore.exceptions import NoCredentialsError
 mime = magic.Magic(mime=True)
 ## Configuration Part 
 BUCKET_NAME = ''
-DELETE_SERVER_FILES = False ## Set False (F should be capital) if you don't want to delete files from bbb-server
+DELETE_SERVER_FILES = True ## Set False (F should be capital) if you don't want to delete files from bbb-server
 
 def upload_to_aws(local_file, bucket, s3_file):
     ctype = mime.from_file(local_file) 
@@ -53,22 +53,23 @@ def main():
     for (dirpath, dirnames, filenames) in os.walk(dirName):
         listOfFiles += [os.path.join(dirpath, file) for file in filenames]
 
-    for elem in listOfFiles: 
+    last_relative_path = "test"
+    for elem in listOfFiles:  
         relative_path = elem.replace(dirName,'')[1:]
         if(relative_path!='bbb-s3.py'):
-            print("\nUploading "+relative_path)    
-            uploaded = upload_to_aws(elem, BUCKET_NAME, relative_path)
-            if(uploaded and DELETE_SERVER_FILES):
-                remove(elem)
+            print("Last path was "+last_relative_path+"\n")
+            print("Current path is "+relative_path.split('/')[-1]+"\n")
+            if(last_relative_path=='metadata.xml' and relative_path.split('/')[-1]=='metadata.xml'):
+                print("This is already uploaded")
             else:
-                print("\nNot able to delete files from your bbb-server, check file if its uploaded")
-
-    file_length = len(listOfFiles)
-    if(file_length==1):
-        for folder in listOfDirs:
-            remove(folder)
-
-        
+                print("\nUploading "+relative_path)    
+                uploaded = upload_to_aws(elem, BUCKET_NAME, relative_path)
+                if(uploaded and DELETE_SERVER_FILES):
+                    if "metadata.xml" not in elem:
+                        remove(elem)
+                else:
+                    print("\nNot able to delete files from your bbb-server, check file if its uploaded")
+        last_relative_path = relative_path.split('/')[-1]
         
         
         
